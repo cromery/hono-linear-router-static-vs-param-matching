6d826fac19fe0ea88f944d083ab75f444ed19ab1
cromery <ek.christian@icloud.com>
2025-12-06 14:04:00 +0100

    fix(linear-router): prevent false matches between static and parameterized routes

--- src/router/linear-router/router.ts
+++ src/router/linear-router/router.ts
@@ -70,6 +70,7 @@ export class LinearRouter<T> implements Router<T> {
         } else if (hasLabel && !hasStar) {
           const params: Record<string, string> = Object.create(null)
           const parts = routePath.match(splitPathRe) as string[]
+          const pathParts = path.match(splitPathRe) || []
 
           const lastIndex = parts.length - 1
           for (let j = 0, pos = 0, len = parts.length; j < len; j++) {
@@ -111,6 +112,9 @@ export class LinearRouter<T> implements Router<T> {
 
               params[name] ||= value as string
             } else {
+              if (pathParts.length < parts.length) {
+                continue ROUTES_LOOP
+              }
               const index = path.indexOf(part, pos)
               if (index !== pos) {
                 continue ROUTES_LOOP
